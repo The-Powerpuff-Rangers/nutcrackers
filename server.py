e9b275ebcecb05ce4eccea9fd8b3f39fe7e909a9
Khushiyant <khushiyant2002@gmail.com>
2023-04-29 07:09:33 +0530

    chore: annotation support--server

--- server.py
+++ server.py
@@ -11,7 +11,8 @@ import base64
 app = flask.Flask(__name__)
 
 # Initialize the YOLO model
-model = YOLO("model/yolov8_v2.pt", agnostic_nms=True)
+model = YOLO("model/yolov8_v2.pt")
+
 
 def _decode(img: str) -> np.ndarray:
     """Decode the image from base64 format.
@@ -20,13 +21,14 @@ def _decode(img: str) -> np.ndarray:
     Returns:
         The decoded image.
     """
-    nparr = np.frombuffer(request.files["image"].read(), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img
+
+    decodedData = base64.b64decode((img))
+    return cv2.imdecode(np.frombuffer(decodedData, np.uint8), cv2.IMREAD_COLOR)
+
 
 def _encode(img: np.ndarray) -> str:
     """Encode the image to base64 format.
-    
+
     Args:
         img: The image to be encoded.
     Returns:
@@ -34,6 +36,7 @@ def _encode(img: np.ndarray) -> str:
     """
     _, buffer = cv2.imencode(".jpg", img)
     return base64.b64encode(buffer).decode("utf-8")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -44,18 +47,36 @@ def predict():
     # Decode the image
     data = request.get_json()
     img = _decode(data['image'])
-    # Predict the bounding boxes
-    result = model(img)[0]
 
-    frame = None
-    
+    # Predict the bounding boxes
+    result = model(img, agnostic_nms=True)[0]
+
+    box_annotator = sv.BoxAnnotator(
+        text_thickness=2,
+        thickness=2,
+        text_scale=1
+    )
+
+    detections = sv.Detections.from_yolov8(result)
+
+    frame = box_annotator.annotate(
+        scene=img, detections=detections)
 
     # Encode the image
-    img = _encode(frame)
+    img_base64 = _encode(frame)
 
     typeA = typeB = typeC = 0
+
+    for box in result.boxes:
+        if box.cls == 0:
+            typeA += 1
+        elif box.cls == 1:
+            typeB += 1
+        elif box.cls == 2:
+            typeC += 1
+
     context = {
-        "image": img,
+        "image": img_base64,
         "data": [
             {
                 "label": "Type A",
@@ -71,4 +92,7 @@ def predict():
             },
         ]
     }
-    return jsonify(context)
+    return context
+
+if __name__ == "__main__":
+    app.run(debug=True, host='10.100.40.135')
